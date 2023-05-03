ebd4ea554e3b012899087b991f80279cc16ffff6
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-05-03 19:54:57 +0900

    remove file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,8 +6,7 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/button.tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,8 +14,7 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/_404.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/button.tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
