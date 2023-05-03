882c285921bc26a552352bbcb2f9972cbb0f4479
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-05-03 19:44:40 +0900

    :)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+import { JSX } from "preact";
 import { Head } from "$fresh/src/runtime/head.ts";
 import { Handlers,PageProps } from "$fresh/server.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
@@ -6,7 +7,6 @@ interface Card{
   name: string;
   img_url: string;
 }
-
 
 export const handler: Handlers<Card[]> = {
   async GET(_,ctx){
@@ -24,29 +24,26 @@ export const handler: Handlers<Card[]> = {
     ];
     return await ctx.render(card);
   }, 
-  
 }
 
 
-export default function Home({data}:PageProps<Card[]>) {
+export default function Page({data}:PageProps<Card[]>) {
   const api_key= config()["API"];
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <div>
-        <h1>my collections</h1>
+      <div class="flex justify-center items-center h-screen">
+        <div class="mb-16 mx-8 text-center">
+        <span class="text-3xl font-bold text-black mb-3">my collections</span>
         {data.map((card)=>(
-          <div>
-            <h2><a href={card.name}>{card.name}</a></h2>
-            <img
-            src= {card.img_url}
-            class="w-32 h-32"
-            alt="card image"
-            />
+          <div class="block m-3" style="position:relative;">
+            <img src= {card.img_url} class="" style="width:100%" alt="item image"/>
+            <div class="bg-gray-300 font-bold" style="position:absolute; top:8px; left:16px;">{card.name}</div>
           </div>
-        ))}
+        ))}        
+        </div>
       </div>
 
 
