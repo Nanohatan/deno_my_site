93463d70c61f47851c2b5f9f2bd707d0d89cf2ef
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-06-01 21:24:29 +0900

    add route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,18 +6,22 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/_app.tsx";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/api/questions.ts";
-import * as $5 from "./routes/index.tsx";
+import * as $3 from "./routes/api/gapi.ts";
+import * as $4 from "./routes/api/joke.ts";
+import * as $5 from "./routes/api/questions.ts";
+import * as $6 from "./routes/index.tsx";
+import * as $7 from "./routes/test/v1.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/_404.tsx": $1,
     "./routes/_app.tsx": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/api/questions.ts": $4,
-    "./routes/index.tsx": $5,
+    "./routes/api/gapi.ts": $3,
+    "./routes/api/joke.ts": $4,
+    "./routes/api/questions.ts": $5,
+    "./routes/index.tsx": $6,
+    "./routes/test/v1.tsx": $7,
   },
   islands: {},
   baseUrl: import.meta.url,
