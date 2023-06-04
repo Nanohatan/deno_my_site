933bdce9166c140ab5ccb9b4d7fd7b41fbf04558
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-06-04 17:48:26 +0900

    use api

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,33 +1,44 @@
 import { Head } from "$fresh/src/runtime/head.ts";
 import { Handlers,PageProps } from "$fresh/server.ts";
-import {getToken,  GoogleAuth,} from "https://deno.land/x/googlejwtsa@v0.1.8/mod.ts";
-
-interface Card{
-  id: string;
-  name: string;
-  img_url: string;
+import * as dejs from "https://deno.land/x/dejs@0.10.3/mod.ts";
+interface Progress {
+  date: string;
+  フィットボクシング: string;
 }
 
-export const handler: Handlers<Card[]> = {
-  async GET(_,ctx){
-    const card: Card[] = [
-      {
-        id:"1",
-        name:"てすてす1",
-        img_url:"https://dummyimage.com/300.png/09f/fff",
-      },
-      {
-        id:"2",
-        name:"てすてす2",
-        img_url:"https://dummyimage.com/300.png/09f/fff",
-      }
-    ];
-    return await ctx.render(card);
-  }, 
-}
+export const handler: Handlers<Progress | null> = {
+  async GET(req, ctx) {
+    console.log(req.url);
+    const resp = await fetch(`${req.url}api/gapi`);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const p: Progress = await resp.json();
+    return ctx.render(p);
+  },
+};
 
+export default function Page({ data }: PageProps<Progress[] | null>) {
+  if (!data) {
+    return <h1>Data not found</h1>;
+  }
+  (async () => {
+    const output = await dejs.renderFile("./static/ejs/template.ejs", {
+      name: "aaa",
+    });
+    await Deno.copy(output,Deno.stdout);
+  })();
+  
+  // return dejs.renderFile("./static/ejs/template.ejs",
+  //   {
+  //     user: "from test2",
+  //     name: "world",
+  //     age: 18,
+  //   },
+  // );
 
-export default function Page({data}:PageProps<Card[]>) {
+  
+  console.log(data[0]);
   return (
     <>
       <Head>
@@ -41,21 +52,28 @@ export default function Page({data}:PageProps<Card[]>) {
       <div class="col card" id="calendar"></div>
       </div>
 
-      <div class="row">
-        <div class="col">
-        <div class="playing-card">
-          <div class="card-inner">
-            <div class="card-front" style="font-size: 5rem;">&#x1f984;
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="64" height="64"><path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path></svg>
-            </div>
-            <div class="card-back"style="font-size: 5rem;">&#x1f98f;</div>
+      <div class="row" >
+        <div class="col" >
+        <div class="card" style="margin:auto; ">
+          <div class="card-body">
+            <h5 class="card-title">&#127939;フィットボクシング</h5>
+            <h6 class="card-subtitle mb-2 text-muted">daily exercise</h6>
+            <p class="card-text">Fit Boxing 2</p>
           </div>
         </div>
         </div>
 
         <div class="col">
-          <div class="card"><span style="font-size: x-large;">&#127939;フィットボクシング</span></div>
+        <div class="card" style="margin:auto; ">
+          <div class="card-body">
+            <h5 class="card-title">&#x1f440;読書、映画鑑賞</h5>
+            <h6 class="card-subtitle mb-2 text-muted">hobby</h6>
+            <a href="#" class="card-link">感想ノート</a>
+          </div>
         </div>
+        </div>
+
+
       </div>
       
       </div>
