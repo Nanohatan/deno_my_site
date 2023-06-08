04a51decafc3335743d364db45696acc42acd47b
homma nonoka <e185730@ie.u-ryukyu.ac.jp>
2023-06-08 23:52:42 +0900

    add

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,8 +1,14 @@
 import {PageProps } from "$fresh/server.ts";
 import {Head} from "$fresh/runtime.ts";
+
+const textResponse = await fetch("https://wakuwaku.deno.dev/api/joke");
+const textData = await textResponse.text();
+console.log(textData);
 export default function Detail(props: PageProps) {
+
   return (
     <>
+    <h3>{textData}</h3>
     <iframe src="test/v1/number-guessing-game.html" width="100%" height="800px"></iframe>
     </>
   )
